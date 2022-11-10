a7f8eb3bff90aab4dedf1fd910c8d274691645d0
RusJam <gregorygreaves@gmil.com>
2022-11-09 19:53:06 -0500

    First commit

---
+++ week-3-js-algorithms/algorithm3.js
@@ -0,0 +1,9 @@
+function sum(a, b){
+            var c = a+b;
+            return c;
+        }
+        
+        const result = (sum(2,2));
+
+        console.log(result);
+        
---
+++ week-3-js-algorithms/test.js
@@ -0,0 +1,17 @@
+//Algorithm 3 - Return Sum of Two Numbers
+
+function multiplyTwoDigits(firstDigit, secondDigit) {
+	return firstDigit * secondDigit;
+}
+	
+	
+	
+	//call the function
+	multiplyTwoDigits(5, 6);
+	const result = multiplyTwoDigits(5, 6);
+	
+	console.log(result);
+
+	{
+	
+}
